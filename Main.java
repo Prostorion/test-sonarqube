3173a1a7f14efdd1e8f2044fd6423f1e633594d2
Prost <prostorion@gmail.com>
2024-03-18 13:40:52 +0200

    first class added

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public String name;
+
+    public Main(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
